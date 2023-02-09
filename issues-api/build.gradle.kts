d1a79d66c4a17f984ed56c70bff14c60d5990abc
leonardo.berlatto <lberlatto@tempo.io>
2023-02-09 14:45:55 -0300

    feat(Issues): add boilerplate issues-api code

---
+++ issues-api/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.2"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "io.board.kaban"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// Spring
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	// database
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	// logging
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+	// utils
+	implementation("org.apache.commons:commons-lang3:3.12.0")
+	// tests
+	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+	testImplementation("io.mockk:mockk:1.13.2")
+	testImplementation("org.assertj:assertj-core:3.24.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ issues-api/src/main/kotlin/io/board/kaban/issues/IssuesApplication.kt
@@ -0,0 +1,11 @@
+package io.board.kaban.issues
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssuesApplication
+
+fun main(args: Array<String>) {
+	runApplication<IssuesApplication>(*args)
+}
