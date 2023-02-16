5c39b3121720e1fd3cae4e8bf8eb994d0f5af330
leonardo.berlatto <lberlatto@tempo.io>
2023-02-16 19:33:55 -0300

    feat(Teams): add get team by id endpoint

--- teams-api/src/main/kotlin/io/board/kanban/teams/adapter/controller/TeamController.kt
+++ teams-api/src/main/kotlin/io/board/kanban/teams/adapter/controller/TeamController.kt
@@ -9,11 +9,13 @@ import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 
 @RestController
@@ -22,6 +24,12 @@ class TeamController(
     val service: TeamService,
     val mapper: TeamMapper
 ) {
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: UUID): TeamResponse {
+        val team = service.findById(id)
+        return mapper.toResponse(team)
+    }
 
     @PostMapping
     fun create(@RequestBody request: CreateTeamRequest): ResponseEntity<TeamResponse> {
