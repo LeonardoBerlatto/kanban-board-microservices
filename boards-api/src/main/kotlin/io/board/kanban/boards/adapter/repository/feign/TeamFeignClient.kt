3446ef5607c2f12a7d315d19be8157bad4e03137
leonardo.berlatto <lberlatto@tempo.io>
2023-03-15 21:24:58 -0300

    fix(Boards): fix error on test initialization

--- boards-api/src/main/kotlin/io/board/kanban/boards/adapter/repository/feign/TeamFeignClient.kt
+++ boards-api/src/main/kotlin/io/board/kanban/boards/adapter/repository/feign/TeamFeignClient.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import java.util.UUID
 
-@FeignClient(name = "team-service", url = "\${api.team.url}")
+@FeignClient(name = "team-service", url = "\${api.teams.url}")
 interface TeamFeignClient {
 
     @GetMapping("/{id}")
