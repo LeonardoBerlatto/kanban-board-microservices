f15ef4595350c9d112bcf3dd58c2f9d012127ae0
LeonardoBerlatto <leoberlatto@gmail.com>
2023-03-16 00:19:06 -0300

    refactor(Boards): convert RabbitMqConfiguration from Java to Kotlin

--- boards-api/src/main/kotlin/io/board/kanban/boards/application/config/RabbitMqConfiguration.kt
+++ boards-api/src/main/kotlin/io/board/kanban/boards/application/config/RabbitMqConfiguration.kt
@@ -1,51 +1,48 @@
-package io.board.kanban.boards.application.config;
+package io.board.kanban.boards.application.config
 
-import io.board.kanban.boards.adapter.broker.rabbitmq.DeleteIssueListener;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.board.kanban.boards.adapter.broker.rabbitmq.DeleteIssueListener
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+const val ISSUE_EXCHANGE = "issue-exchange"
+const val ISSUE_QUEUE_NAME = "issue"
 
 @Configuration
-public class RabbitMqConfiguration {
-    public static final String ISSUE_EXCHANGE = "issue-exchange";
-    public static final String ISSUE_QUEUE_NAME = "issue";
+class RabbitMqConfiguration {
 
     @Bean
-    public Queue queue() {
-        return new Queue(ISSUE_QUEUE_NAME, true);
+    fun queue(): Queue = Queue(ISSUE_QUEUE_NAME, true)
+
+    @Bean
+    fun exchange(): TopicExchange = TopicExchange(ISSUE_EXCHANGE)
+
+    @Bean
+    fun binding(queue: Queue?, exchange: TopicExchange?): Binding = BindingBuilder
+        .bind(queue)
+        .to(exchange)
+        .with("$ISSUE_QUEUE_NAME.inactive")
+
+    @Bean
+    fun container(
+        connectionFactory: ConnectionFactory?,
+        listenerAdapter: MessageListenerAdapter?
+    ): SimpleMessageListenerContainer {
+        val container = SimpleMessageListenerContainer()
+        container.connectionFactory = connectionFactory!!
+        container.setQueueNames(ISSUE_QUEUE_NAME)
+        container.setMessageListener(listenerAdapter!!)
+        return container
     }
 
     @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(ISSUE_EXCHANGE);
-    }
+    fun listenerAdapter(listener: DeleteIssueListener?): MessageListenerAdapter =
+        MessageListenerAdapter(listener, "receiveMessage")
 
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(ISSUE_QUEUE_NAME + ".inactive");
-    }
-
-
-    @Bean
-    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(ISSUE_QUEUE_NAME);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(DeleteIssueListener listener) {
-        return new MessageListenerAdapter(listener, "receiveMessage");
-    }
 }
